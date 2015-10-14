53c5ddb31b338b889e26d0706e8e94306ada170b
zhang <xyzhang0817@gmail.com>
2015-10-14 17:15:38 -0400

    Copying the test and Database helper

--- app/src/androidTest/java/com/example/zhang/sunshine_version3/app/FullTestSuit.java
+++
@@ -1,7 +0,0 @@
-package com.example.zhang.sunshine_version3.app;
-
-/**
- * Created by zhang on 13/10/15.
- */
-public class FullTestSuit {
-}
--- app/src/androidTest/java/com/example/zhang/sunshine_version3/app/data/TestDB.java
+++
@@ -1,7 +0,0 @@
-package com.example.zhang.sunshine_version3.app.data;
-
-/**
- * Created by zhang on 13/10/15.
- */
-public class TestDB {
-}
