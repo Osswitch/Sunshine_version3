7fd043f4310df9524b6bba1938684fdcd5505865
zhang <xyzhang0817@gmail.com>
2015-10-26 20:18:09 -0400

    Detail page shows the message.

--- app/src/main/java/com/example/zhang/sunshine_version3/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/zhang/sunshine_version3/app/DetailActivityFragment.java
@@ -1,20 +1,44 @@
 package com.example.zhang.sunshine_version3.app;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.zhang.sunshine_version3.app.data.WeatherContract;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailActivityFragment extends Fragment {
+public class DetailActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private String detailMessage;
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
 
+    private static final int DETAIL_WEATHER_LOADER_ID = 0;
+
+    private String mForecastStr;
+
+    private static final String[] FORECAST_COLUMNS= {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+
+    };
+
+    private static final int COL_WEATHER_ID = 0;
+    private static final int COL_WEATHER_DATE = 1;
+    private static final int COL_WEATHER_SHORT_DESC = 2;
+    private static final int COL_WEATHER_MAX_TEMP =3;
+    private static final int COL_WEATHER_MIN_TEMP = 4;
 
     public DetailActivityFragment() {
     }
@@ -23,13 +47,60 @@ public class DetailActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        Intent intent = getActivity().getIntent();
-
-        if (intent != null){
-            detailMessage = intent.getDataString();
-            ((TextView) rootView.findViewById(R.id.fragment_detail_textview)).setText(detailMessage);
-        }
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(DETAIL_WEATHER_LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        Intent intent = getActivity().getIntent();
+
+        if (intent == null) {
+            return null;
+        }
+
+        return new CursorLoader(
+                getActivity(),
+                intent.getData(),
+                FORECAST_COLUMNS,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+
+        if (!cursor.moveToFirst()) {
+            return;
+        }
+
+        String date = Utility.formatDate(cursor.getLong(COL_WEATHER_DATE));
+        String weatherDescription = cursor.getString(COL_WEATHER_SHORT_DESC);
+        Boolean isMetric = Utility.isMetric(getActivity());
+        String maxTemperature = Utility
+                .formatTemperature(cursor.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+        String minTemperature = Utility
+                .formatTemperature(cursor.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+
+        mForecastStr = date + " - " + weatherDescription + " - " + maxTemperature
+                + " / " + minTemperature;
+        TextView textView = (TextView) getView().findViewById(R.id.fragment_detail_textView);
+
+        textView.setText(mForecastStr);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
     }
 }
