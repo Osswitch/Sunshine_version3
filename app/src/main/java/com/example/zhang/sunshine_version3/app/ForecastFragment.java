451cbbf141406b59b0985b639d0ca352692edb10
zhang <xyzhang0817@gmail.com>
2015-11-05 16:38:45 -0500

    Implement refreshment by service rather than AsyncTask.

--- app/src/main/java/com/example/zhang/sunshine_version3/app/ForecastFragment.java
+++ app/src/main/java/com/example/zhang/sunshine_version3/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.zhang.sunshine_version3.app;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.zhang.sunshine_version3.app.data.WeatherContract;
+import com.example.zhang.sunshine_version3.app.service.SunshineService;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -230,10 +232,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     public void updateWeather() {
-
-        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        fetchWeatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     public void setUseTodayLayout(boolean useTodayLayout) {
